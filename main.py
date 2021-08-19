a4ada7096d1b164bb881f7124422a3a7024db97e
Alec Goedinghaus <alecgoedinghaus@ucla.edu>
2021-08-19 00:24:26 -0700

    the numbers make sense now

--- main.py
+++ main.py
@@ -5,37 +5,33 @@ import skimage as sk
 from skimage import io, transform, util
 from skimage.util.dtype import img_as_ubyte, img_as_uint
 
-def get_like_colors(image, tolerance, x, y):
-    values = np.zeros([x, y])
+def get_like_colors(image, dimension, tolerance = 100):
+    values = np.zeros([dimension[0], dimension[1]])
     iterator = 0
 
-    for i in range(x):
-        for j in range(y):
+    for i in range(dimension[0]):
+        for j in range(dimension[1]):
             # If values != 0, then this pixel's group has been numbered
             if values[i, j] == 0:
                 iterator += 1
-                for k in range(x):
-                    for l in range(y):
-                        dist = np.linalg.norm(image[i, j] - image[k, l])
-                        if dist < tolerance:
+                for k in range(dimension[0]):
+                    for l in range(dimension[1]):
+                        dist = [abs(int(image[i, j, rgb]) - int(image[k, l, rgb])) for rgb in range(3)]
+                        if all(dist_i < tolerance for dist_i in dist):
                             values[i, j] = iterator
                             values[k, l] = iterator
 
-    return values
+    return values, iterator
 
-def overlay(image, values, x, y):
-    #values = get_like_colors(image, 100, x, y)
+def overlay(image, values, dimension):
     inverted_img = util.invert(image)
 
     # Make this display each different number on separate plots
-    for i in range(x):
-        for j in range(y):
-            plt.text(-0.5 + i, 0.5 + j, str(int(values[i, j])), color = inverted_img[j, i] / 255, size = 6)
-        
-    # plt.text(-0.5 + 5, 0.5 + 30, '1', color = inverted_img[30, 5] / 255, size = 8)
+    for i in range(dimension[0]):
+        for j in range(dimension[1]):
+            plt.text(-0.5 + i, 0.5 + j, str(int(values[j, i])), color = inverted_img[j, i] / 255, size = 6)
 
 def save_and_display(image, name):
-    # io.imsave(fname = name + '.png', arr = image)
     io.imshow(image)
     plt.savefig(name + '_plt.png', bbox_inches = 'tight')
     plt.show()
@@ -48,8 +44,8 @@ def main():
     rescaled_bust = transform.downscale_local_mean(bust, (10, 10, 1), cval = 1)
     rescaled_bust = img_as_ubyte(rescaled_bust / 255)
 
-    values = get_like_colors(rescaled_bust, 50, 32, 32)
-    overlay(rescaled_bust, values, 32, 32)
+    values, num_plots = get_like_colors(rescaled_bust, (32, 32), tolerance = 25)
+    overlay(rescaled_bust, values, (32, 32))
     save_and_display(rescaled_bust, 'output')
 
 main()
